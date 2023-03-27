7e7aa4bd07f9cfa845deed41ad57c071a3d971b5
Michal Perdek <115001860+michal-perdek-cpg@users.noreply.github.com>
2023-03-27 15:08:50 +0200

    Basic setup of scenes

---
+++ Assets/Scripts/RunTime/Installers/GameInstaller.cs
@@ -0,0 +1,11 @@
+using Zenject;
+
+namespace RunTime.Installers
+{
+    public class GameInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+        }
+    }
+}
---
+++ Assets/Scripts/RunTime/Managers/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace RunTime.Managers
+{
+    public class GameManager : MonoBehaviour
+    {
+        #region MEMBERS
+    
+        #endregion
+    
+        #region PROPERTIES
+    
+        #endregion
+    
+        #region UNITY_METHODS
+
+        private void Awake()
+        {
+        
+        }
+
+        #endregion
+
+        #region METHODS
+
+        #endregion
+    }
+
+}
