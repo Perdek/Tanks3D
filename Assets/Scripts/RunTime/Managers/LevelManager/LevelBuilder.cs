a3c066c50fc5870694a77287577137a687477e09
Michal Perdek <115001860+michal-perdek-cpg@users.noreply.github.com>
2023-04-06 14:58:16 +0200

    Add LevelManager

---
+++ Assets/Scripts/RunTime/Managers/LevelManager/LevelBuilder.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace RunTime.Managers
+{
+    public class LevelBuilder
+    {
+        #region MEMBERS
+
+        private Transform _parentToSpawnLevel;
+
+        #endregion
+
+        #region METHODS
+
+        public LevelBuilder(Transform parentToSpawnLevel)
+        {
+            _parentToSpawnLevel = parentToSpawnLevel;
+        }
+        
+        public void CreateLevel()
+        {
+            
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Scripts/RunTime/Managers/LevelManager/LevelManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+namespace RunTime.Managers
+{
+    public class LevelManager
+    {
+        #region MEMBERS
+
+        [SerializeField] private Transform _parentToSpawnLevel;
+        private LevelBuilder _levelBuilder;
+
+        #endregion
+
+        #region UNITY_METHODS
+
+        private void Awake()
+        {
+            Init();
+            SpawnMap();
+        }
+
+        #endregion
+
+        #region METHODS
+        
+        private void Init()
+        {
+            _levelBuilder = new LevelBuilder(_parentToSpawnLevel);
+        }
+
+        private void SpawnMap()
+        {
+            _levelBuilder.CreateLevel();
+        }
+
+        #endregion
+    }
+}
