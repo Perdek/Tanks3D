c5bcf82a093126818de79fb6fe840eaa15d1189f
Michal Perdek <115001860+michal-perdek-cpg@users.noreply.github.com>
2023-05-15 15:08:28 +0200

    [WIP] MapGeneratorEditor

--- Assets/Scripts/Editor/MapGeneratorModule/MapGenerator.cs
+++ Assets/Scripts/Editor/MapGeneratorModule/MapGenerator.cs
@@ -1,21 +1,46 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace Editor.MapGeneratorModule
 {
+    [Serializable]
+    public class MapTileSetup
+    {
+        public MapTile.MapTileEnum MapTileType { get; set; }
+        public char Character { get; set; }
+        public float Percent { get; set; }
+    }
+
     public class MapGenerator : MonoBehaviour
     {
         #region MEMBERS
 
-        [SerializeField] private List<MapTile> _mapTiles;
+        [SerializeField] private List<MapTileSetup> _mapTileSetups = new List<MapTileSetup>();
+
+        private int _mapSizeWidth = 17;
+        private int _mapSizeHeight = 17;
 
         #endregion
 
         #region PROPERTIES
 
-        public int MapSizeWidth { get; set; } = 17;
+        public int MapSizeWidth
+        {
+            get { return _mapSizeWidth; }
+            set { _mapSizeWidth = Mathf.Max(0, value); }
+        }
 
-        public int MapSizeHeight { get; set; } = 17;
+        public int MapSizeHeight
+        {
+            get { return _mapSizeHeight; }
+            set { _mapSizeHeight = Mathf.Max(0, value); }
+        }
+
+        public List<MapTileSetup> MapTileSetups
+        {
+            get { return _mapTileSetups; }
+        }
 
         #endregion
     }
--- Assets/Scripts/Editor/MapGeneratorModule/MapGeneratorEditor.cs
+++ Assets/Scripts/Editor/MapGeneratorModule/MapGeneratorEditor.cs
@@ -9,8 +9,17 @@ namespace Editor.MapGeneratorModule
         private const int LabelWidth = 50;
         private const int FieldWidth = 30;
 
+        private SerializedProperty _mapTileSetupsProperty;
+
+        private void OnEnable()
+        {
+            _mapTileSetupsProperty = serializedObject.FindProperty("_mapTileSetups");
+        }
+
         public override void OnInspectorGUI()
         {
+            serializedObject.Update();
+
             // Reference to the target script
             MapGenerator mapGenerator = (MapGenerator)target;
 
@@ -28,6 +37,9 @@ namespace Editor.MapGeneratorModule
             EditorGUILayout.LabelField("Width", GUILayout.Width(LabelWidth));
             mapGenerator.MapSizeWidth = Mathf.Clamp(EditorGUILayout.IntField(mapGenerator.MapSizeWidth, GUILayout.Width(FieldWidth)), 0, 50);
 
+            // Add flexible space to dynamically adjust the spacing
+            GUILayout.FlexibleSpace();
+
             // Create custom GUI field for height with limited width
             EditorGUILayout.LabelField("Height", GUILayout.Width(LabelWidth));
             mapGenerator.MapSizeHeight = Mathf.Clamp(EditorGUILayout.IntField(mapGenerator.MapSizeHeight, GUILayout.Width(FieldWidth)), 0, 50);
@@ -37,6 +49,44 @@ namespace Editor.MapGeneratorModule
 
             // End the grouped box
             EditorGUILayout.EndVertical();
+
+            EditorGUILayout.Space();
+
+            // Display the map tile setups list
+            EditorGUILayout.LabelField("Map Tile Setups", EditorStyles.boldLabel);
+            EditorGUI.indentLevel++;
+
+            // Display the array size field
+            EditorGUILayout.PropertyField(_mapTileSetupsProperty, new GUIContent("Size"));
+
+            if (_mapTileSetupsProperty.isExpanded)
+            {
+                // Display the individual map tile setups fields
+                for (int i = 0; i < _mapTileSetupsProperty.arraySize; i++)
+                {
+                    SerializedProperty mapTileSetupProperty = _mapTileSetupsProperty.GetArrayElementAtIndex(i);
+
+                    EditorGUILayout.BeginHorizontal();
+
+                    // MapTileType field
+                    SerializedProperty mapTileTypeProperty = mapTileSetupProperty.FindPropertyRelative("MapTileType");
+                    EditorGUILayout.PropertyField(mapTileTypeProperty, GUIContent.none, GUILayout.Width(120));
+
+                    // Character field
+                    SerializedProperty characterProperty = mapTileSetupProperty.FindPropertyRelative("Character");
+                    EditorGUILayout.PropertyField(characterProperty, GUIContent.none, GUILayout.Width(30));
+
+                    // Percentage field
+                    SerializedProperty percentProperty = mapTileSetupProperty.FindPropertyRelative("Percent");
+                    EditorGUILayout.PropertyField(percentProperty, GUIContent.none, GUILayout.Width(50));
+
+                    EditorGUILayout.EndHorizontal();
+                }
+            }
+
+            EditorGUI.indentLevel--;
+
+            serializedObject.ApplyModifiedProperties();
         }
     }
 }
